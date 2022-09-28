0dcf1a5b55576b9b6dd20330a22f833398ea757d
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-28 18:39:27 +0700

    added for loop in other version

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,6 +1,17 @@
 public class ForLoop {
   public static void main(String[] args) {
 
+    var num = 1;
+    for (; num <=5; ) {
+      System.out.println("Num " + num);
+      num++;
+    }
+
+    for(var ber = 1; ber <= 7; ) {
+      System.out.println("Number " + ber);
+      ber++;
+    }
+
     for (var counter = 1; counter <= 10; counter++) {
       System.out.println("Perulangan " + counter);
     }
